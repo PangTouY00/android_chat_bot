51ffdf61412d4ea748227ec7d8782fb49c561899
PangTouY00 <164233995+PangTouY00@users.noreply.github.com>
2024-11-09 19:08:00 +0800

    Delete app directory

--- app/src/main/java/com/example/pythoneditor/model/VersionInfo.kt
+++
@@ -1,9 +0,0 @@
-package com.example.pythoneditor.model
-
-data class VersionInfo(
-    val versionCode: Int,
-    val versionName: String,
-    val updateMessage: String,
-    val downloadUrl: String,
-    val forceUpdate: Boolean
-) 
